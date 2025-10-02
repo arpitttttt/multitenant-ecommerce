72dd29938f740758dec8d52605e2e33810eb0a6f
Arpit Singh <121520688+arpitttttt@users.noreply.github.com>
2025-10-02 06:04:32 +0530

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Progress } from "@/components/ui/progress"
+import { Textarea } from "@/components/ui/textarea"
+import { Checkbox } from "@/components/ui/checkbox"
 
-export default function Page() {
+export default function Home() {
   return (
-    <div className="p-10 space-y-4">
-      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind</h1>
-      <Button>
-        Green Button
-      </Button>
+    <div className='p-4'>
+   <div className="flex flex-col gap-y-4">
+    <div>
+      <Button variant="elevated">
+        I am a button
+        </Button>
     </div>
+    <div>
+      <Input placeholder="I am input" />
+    </div>
+    <div>
+      <Progress value={50} />
+    </div>
+    <div>
+      <Textarea value="I am a textarea" />
+    </div>
+    <div>
+      <Checkbox />
+    </div>
+   </div>
+   </div>
   )
-}
+} 
